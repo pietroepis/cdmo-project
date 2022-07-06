1d09e2b756a105ff62a23384120fe0805295b08f
Pietro <pietroepis@gmail.com>
2022-07-06 11:33:02 +0200

    Preprocessing - Sort

--- CP/main.py
+++ CP/main.py
@@ -6,7 +6,7 @@ from datetime import timedelta
 
 path = os.path.abspath(os.path.dirname(__file__))
 
-filename = "ins-" + (str(sys.argv[1]) if len(sys.argv) == 2 else "1") + ".txt"
+filename = "ins-" + (str(sys.argv[1]) if len(sys.argv) == 2 else "11") + ".txt"
 allow_rotation = False
 
 model = Model("./model_rotation.mzn" if allow_rotation else "./model.mzn")
@@ -16,13 +16,16 @@ instance = Instance(solver, model)
 
 f = open(os.path.join(path, "..\\instances\\" + filename), "r")
 input = f.read().splitlines()
+input[2:] = sorted(input[2:], key = lambda item: int(item.split(" ")[0]) * int(item.split(" ")[1]), reverse = True)
 instance["width"] = int(input[0])
 instance["n"] = int(input[1])
-instance["dimensions"] = [tuple(map(int, input[i + 2].split(" "))) for i in range(instance["n"])]
+instance["w"] = [tuple(map(int, input[i + 2].split(" ")))[0] for i in range(instance["n"])]
+instance["h"] = [tuple(map(int, input[i + 2].split(" ")))[1] for i in range(instance["n"])]
+
 result = instance.solve(timeout=timedelta(seconds=301))
 # result = instance.solve()
 
-dimensions = instance["dimensions"] if not allow_rotation else result["actual_dimensions"]
+dimensions = list(zip(instance["w"], instance["h"])) if not allow_rotation else result["actual_dimensions"]
 
 result_out = input[0] + " " + str(result["height"]) + "\n" + input[1] + "\n"
 for i in range(int(input[1])):
